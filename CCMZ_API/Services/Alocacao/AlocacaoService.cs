b4e3753c031d976b22383b8e74da8137729e4024
Pedro H. Alarcon <pedrohenriquealarcon@gmail.com>
2023-11-07 21:42:17 -0300

    Adicionado nova rota (rota de Comunidade aonde filtra por código do evento)  - 2023.11.7.2

--- CCMZ_API/Services/Alocacao/AlocacaoService.cs
+++ CCMZ_API/Services/Alocacao/AlocacaoService.cs
@@ -37,11 +37,16 @@ public class AlocacaoService : IAlocacaoService
 
     public async Task<IEnumerable<ComunidadeNome>> GetComunidades(int codigoEvento)
     {
-        return await _context.TbComunidades.Select(c => new ComunidadeNome
-        {
-            ComCodigo = c.ComCodigo,
-            ComNome = c.ComNome
-        }).ToListAsync();
+        return await _context.TbComunidades
+            .Join(_context.TbPessoas, c => c.ComCodigo, p => p.ComCodigo, (c, p) => new {c, p})
+            .Join(_context.TbEventoPessoas, x => x.p.PesCodigo, ep => ep.PesCodigo, (x, ep) => new {x, ep})
+            .Join(_context.TbEventos, z => z.ep.EveCodigo, e => e.EveCodigo, (z, e) => new {z, e})
+            .Where(y => y.e.EveCodigo == codigoEvento)
+            .Select(y => new ComunidadeNome
+            {
+                ComCodigo = y.z.x.c.ComCodigo,
+                ComNome = y.z.x.c.ComNome
+            }).Distinct().ToListAsync();
     }
 
     public async Task<IEnumerable<QuartosNome>> GetQuartos(int codigoEvento)
