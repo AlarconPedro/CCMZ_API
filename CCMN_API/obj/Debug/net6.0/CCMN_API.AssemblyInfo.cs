586da1aec633ae37b889192f5ec66929bac05248
Pedro H. Alarcon <pedrohenriquealarcon@gmail.com>
2024-04-28 11:08:46 -0300

    Subido para o servidor - 2024.4.28.1

--- CCMN_API/obj/Debug/net6.0/CCMN_API.AssemblyInfo.cs
+++ CCMN_API/obj/Debug/net6.0/CCMN_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CCMN_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d17d9e9af7384b55bd2aa36f97940d24e509acd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52310f8a371da58e99f1d00083aa3c8c77224e54")]
 [assembly: System.Reflection.AssemblyProductAttribute("CCMN_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CCMN_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
